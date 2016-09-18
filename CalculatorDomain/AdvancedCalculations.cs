99c04e4cad2b5b7da3394d05c290b634e8d662a2
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2016-09-18 14:49:02 +0200

    Made internal methods visibible to test classes

--- CalculatorDomain/AdvancedCalculations.cs
+++ CalculatorDomain/AdvancedCalculations.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+
+[assembly: InternalsVisibleTo("NameOfTestAssembly")]
 
 namespace CalculatorDomain
 {
